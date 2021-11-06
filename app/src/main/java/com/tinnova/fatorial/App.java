faee7a89152c463ac576bc0b815f8aad65c7016b
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-11-06 10:59:24 -0300

    Clean initial project

--- app/src/main/java/com/tinnova/fatorial/App.java
+++ app/src/main/java/com/tinnova/fatorial/App.java
@@ -4,11 +4,8 @@
 package com.tinnova.fatorial;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
     }
 }
